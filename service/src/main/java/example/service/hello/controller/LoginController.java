5212146b8e78f62b3cfddc5f61a5ccd3e0f74a63
gregwhitaker <gwhitake@gmail.com>
2019-12-16 13:58:06 -0800

    Doing work

--- service/src/main/java/example/service/hello/controller/LoginController.java
+++ service/src/main/java/example/service/hello/controller/LoginController.java
@@ -3,16 +3,16 @@ package example.service.hello.controller;
 import example.service.hello.controller.model.LoginRequest;
 import example.service.hello.controller.model.LoginResponse;
 import example.service.hello.service.AuthService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
 
+/**
+ * Controller that handles authenticating users.
+ */
 @Controller
 public class LoginController {
-    private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
 
     private final AuthService authService;
 
@@ -20,6 +20,13 @@ public class LoginController {
         this.authService = authService;
     }
 
+    /**
+     * Checks the username and password with the system. If correct, this
+     * method issues a JWT token for authenticating with the service.
+     *
+     * @param request username and password
+     * @return a {@link LoginResponse} if the login was successful
+     */
     @MessageMapping("login")
     public Mono<LoginResponse> login(LoginRequest request) {
         return authService.login(request.getUsername(), request.getPassword())
@@ -31,6 +38,12 @@ public class LoginController {
                 });
     }
 
+    /**
+     * Logs the specified user out of the system.
+     *
+     * @param token jwt token of the user to log out
+     * @return an empty mono if successful
+     */
     @MessageMapping("logout")
     public Mono<?> logout(@Header("token") String token) {
         return authService.authorize(token)
