813f1be4e1a1ffbe262800f14c9839ad39275312
gregwhitaker <gwhitake@gmail.com>
2019-12-16 13:59:34 -0800

    Doing work

--- service/src/main/java/example/service/hello/service/AuthService.java
+++ service/src/main/java/example/service/hello/service/AuthService.java
@@ -5,6 +5,9 @@ import example.service.hello.core.auth.TokenAuthorizer;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+/**
+ * Service that handles authentication with the system.
+ */
 @Component
 public class AuthService {
 
@@ -14,14 +17,31 @@ public class AuthService {
         this.tokenAuthorizer = tokenAuthorizer;
     }
 
+    /**
+     * Check that the token is authorized to access the system.
+     *
+     * @param token jwt token to check
+     * @return a {@link DecodedJWT} if successful
+     */
     public Mono<DecodedJWT> authorize(String token) {
         return tokenAuthorizer.authorize(token);
     }
 
+    /**
+     *
+     * @param username
+     * @param password
+     * @return
+     */
     public Mono<String> login(String username, String password) {
         return null;
     }
 
+    /**
+     * 
+     * @param decodedJWT
+     * @return
+     */
     public Mono<?> logout(DecodedJWT decodedJWT) {
         return null;
     }
