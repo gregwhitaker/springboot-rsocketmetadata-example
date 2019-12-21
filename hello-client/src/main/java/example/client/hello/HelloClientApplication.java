770b09b4ab42b4ccf91c2d0c8f0c746ed449f46f
gregwhitaker <gwhitake@gmail.com>
2019-12-20 17:55:39 -0800

    Doing work

--- hello-client/src/main/java/example/client/hello/HelloClientApplication.java
+++ hello-client/src/main/java/example/client/hello/HelloClientApplication.java
@@ -1,12 +1,26 @@
 package example.client.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class HelloClientApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloClientApplication.class);
 
     public static void main(String... args) {
         SpringApplication.run(HelloClientApplication.class, args);
+    }
+
+    @Component
+    public class Runner implements CommandLineRunner {
+
+        @Override
+        public void run(String... args) throws Exception {
+
+        }
     }
 }
